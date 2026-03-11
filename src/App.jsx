bd6e5165607b4d387d7ce12d7276319a868f552a
vijay_36 <dvj.36b@gmail.com>
2026-03-11 12:46:39 +0530

    deploying to render

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import CarouselComponent from "./components/CarouselComponent"
+import Footer from "./components/Footer"
+import FooterDetails from "./components/FooterDetails"
+import Growth from "./components/Growth"
+import TagLine from "./components/TagLine"
+import TeamMembers from "./components/TeamMembers"
+import VisionMissionAims from "./components/VisionMissionAims"
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import HomePage from "./pages/HomePage"
+import ServicesPage from "./pages/ServicesPage"
+import ContactPage from "./pages/ContactPage"
+import AboutPage from "./pages/AboutPage"
+import NavBar from "./Components/Navbar"
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+          <NavBar/>
+          <Routes>
+             <Route path="/" element={<HomePage />}></Route>
+             <Route path="/services" element={<ServicesPage />}></Route>
+             <Route path="/contact" element={<ContactPage />}></Route>
+             <Route path="/about" element={<AboutPage />}></Route>
+          </Routes>
+          <FooterDetails />
+          <Footer />
+      </BrowserRouter>
+      
+      
+      
+    </>
+  )
+}
+
+export default App
